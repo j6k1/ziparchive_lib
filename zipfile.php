502592675283766851518ca3939e5ebc439c57f2
jinpu <jinpu@s254.xrea.com>
2012-04-08 14:08:28 +0900

    コメントのバージョン番号を更新

--- zipfile.php
+++ zipfile.php
@@ -3,7 +3,7 @@
  * ZipFile
  *
  * @package ZipFile - zip archive
- * @version 0.0.6-alpha
+ * @version 0.0.7-alpha
  * @author jinpu <http://will-co21.net>
  * @lisence The LGPL License
  * @copyright Copyright 2012 jinpu. All rights reserved.
